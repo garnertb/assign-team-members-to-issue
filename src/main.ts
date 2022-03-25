fb61de9f1ef088257a461b3da6cb3b58a335b463
Tyler Garner <garnertb@github.com>
2022-03-25 10:42:30 -0400

    initial work

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,54 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {Octokit} from '@octokit/rest'
+// eslint-disable-next-line import/no-unresolved
+import {OctokitOptions} from '@octokit/core/dist-types/types'
+
+const newClient = (options?: OctokitOptions): Octokit => {
+  return new Octokit(options)
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const org: string = core.getInput('org')
+    const team_slug: string = core.getInput('team')
+    const repo: string = core.getInput('repository')
+    const issue_number: number = parseInt(core.getInput('issue_number'))
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(
+      `Org: ${org}, team:${team_slug}, repo: ${repo}, issue_number: ${issue_number}`
+    )
 
-    core.setOutput('time', new Date().toTimeString())
+    const client = newClient({auth: core.getInput('token')})
+
+    const {data: members} = await client.teams.listMembersInOrg({
+      org,
+      team_slug
+    })
+
+    const issue = await client.issues.get({
+      owner: org,
+      repo,
+      issue_number
+    })
+
+    // you can only assign up to 10 people to an issue
+    // check how much space is left
+    const spaceAvailable = issue.data.assignees
+      ? 10 - issue.data.assignees.length
+      : 10
+
+    core.debug(`Space available for assignees: ${spaceAvailable}`)
+
+    const assignees = members.slice(0, spaceAvailable - 1).map(a => a.login)
+
+    core.debug(`Members that will be assigned: ${assignees}`)
+
+    await client.issues.addAssignees({
+      owner: org,
+      repo,
+      issue_number,
+      assignees
+    })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
